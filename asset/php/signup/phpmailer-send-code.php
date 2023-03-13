c912a64863fee4fc715a2decdfefdbb1148bef5a
Andre <andrecausing343@gmail.com>
2023-03-13 16:48:32 +0800

    .

--- asset/php/signup/phpmailer-send-code.php
+++ asset/php/signup/phpmailer-send-code.php
@@ -19,50 +19,56 @@
 
             if(isset($_SESSION["signUpUserEmail"])){
                 $email = $_SESSION["signUpUserEmail"];
-
-                $check_email_table = mysqli_query($classConnDB->conn(), "SELECT * FROM users_tbl WHERE email ='$email'");
-                while($row = mysqli_fetch_assoc($check_email_table)){
-                    
-                    //Create an instance; passing `true` enables exceptions
-                    $mail = new PHPMailer(true);
-
-                    try {
-                        //Server settings
-                        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-                        $mail->isSMTP();                                            //Send using SMTP
-                        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-                        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                        $mail->Username   = 'shawngomo@gmail.com';                     //SMTP username
-                        $mail->Password   = 'qrcjdthuealfhcnl';                               //SMTP password
-                        $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-                        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-                        //Recipients
-                        $mail->setFrom('from@example.com', 'Mailer');
-                        $mail->addAddress($email, 'Joe User');     //Add a recipient
-
-                        // $mail->addAddress('ellen@example.com');               //Name is optional
-                        // $mail->addReplyTo('info@example.com', 'Information');
-                        // $mail->addCC('cc@example.com');
-                        // $mail->addBCC('bcc@example.com');
-
-                        //Attachments
-                        // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-                        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-                        //Content
-                        $mail->isHTML(true);                                  //Set email format to HTML
-                        $mail->Subject = 'Here is the subject';
-                        $mail->Body    = 'This is the HTML message body <b>' .$row["verification_code"] .'</b>';
-                        $mail->AltBody = $row["verification_code"];
-
-                        if($mail->send())
-                            echo 'Message has been sent';
-                    } catch (Exception $e) {
-                        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            
+                // Sanitize user input using prepared statements
+                $stmt = $classConnDB->conn()->prepare("SELECT * FROM users_tbl WHERE email = ?");
+                $stmt->bind_param("s", $email);
+                $stmt->execute();
+                $result = $stmt->get_result();
+            
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()){
+            
+                        //Create an instance; passing `true` enables exceptions
+                        $mail = new PHPMailer(true);
+            
+                        try {
+                            //Server settings
+                            $mail->isSMTP();                                            //Send using SMTP
+                            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+                            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                            $mail->Username   = 'shawngomo@gmail.com';                     //SMTP username or API key
+                            $mail->Password   = 'qrcjdthuealfhcnl';                               //SMTP password or API secret
+                            $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+                            $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            
+                            //Recipients
+                            $mail->setFrom('from@example.com', 'Mailer');
+                            $mail->addAddress($email, 'Joe User');     //Add a recipient
+            
+                            //Content
+                            $mail->isHTML(true);                                  //Set email format to HTML
+                            $mail->Subject = 'Here is the subject';
+                            $mail->Body    = 'This is the HTML message body <b>' .$row["verification_code"] .'</b>';
+                            $mail->AltBody = $row["verification_code"];
+            
+                            if($mail->send()) {
+                                echo 'Message has been sent';
+                            } else {
+                                echo 'Message could not be sent';
+                            }
+                        } catch (Exception $e) {
+                            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                        }
                     }
+                } else {
+                    echo "User with email $email not found.";
                 }
+            
+                $stmt->close();
             }
+            
+            
         }
     }
 ?>
