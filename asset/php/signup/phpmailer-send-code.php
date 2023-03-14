7b29a509b5355a3686dc1941bb89c72f8f991694
andrecausingg <95618374+andrecausingg@users.noreply.github.com>
2023-03-14 13:06:01 +0800

    .

--- asset/php/signup/phpmailer-send-code.php
+++ asset/php/signup/phpmailer-send-code.php
@@ -5,18 +5,21 @@
     use PHPMailer\PHPMailer\SMTP;
     use PHPMailer\PHPMailer\Exception;
 
+    $classSendVerCodeEmail = new classSendVerCodeEmail();
+    $classSendVerCodeEmail->sendVerCodeEmail();
+
     class classSendVerCodeEmail{
         function sendVerCodeEmail(){
             //Load Composer's autoloader
-            // require_once '../helper/library/PHPMailer/vendor/autoload.php';
             require_once '../helper/vendor/autoload.php';
             // Database Connection
             require_once '../helper/global/global.php';
-
             // Sign Up session
             require_once 'signup.php';
 
+            // Class
             $classConnDB = new classConnDB();
+
 
             if(isset($_SESSION["signUpUserEmail"])){
                 $email = $_SESSION["signUpUserEmail"];
@@ -29,35 +32,41 @@
             
                 if ($result->num_rows > 0) {
                     while($row = $result->fetch_assoc()){
-            
+                        
                         //Create an instance; passing `true` enables exceptions
                         $mail = new PHPMailer(true);
-            
+                        
                         try {
                             //Server settings
+                            // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
                             $mail->isSMTP();                                            //Send using SMTP
                             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                            $mail->Username   = 'shawngomo@gmail.com';                     //SMTP username or API key
-                            $mail->Password   = 'qrcjdthuealfhcnl';                               //SMTP password or API secret
+                            $mail->Username   = 'shawngomo@gmail.com';                     //SMTP username
+                            $mail->Password   = 'qrcjdthuealfhcnl';                               //SMTP password
                             $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
                             $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-            
+                        
                             //Recipients
                             $mail->setFrom('from@example.com', 'Mailer');
-                            $mail->addAddress($email, 'Joe User');     //Add a recipient
-            
+                            $mail->addAddress('andrecausing33@gmail.com', 'Joe User');     //Add a recipient
+                            $mail->addAddress('ellen@example.com');               //Name is optional
+                            $mail->addReplyTo('info@example.com', 'Information');
+                            $mail->addCC('cc@example.com');
+                            $mail->addBCC('bcc@example.com');
+                        
+                            //Attachments
+                            // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+                            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+                        
                             //Content
                             $mail->isHTML(true);                                  //Set email format to HTML
                             $mail->Subject = 'Here is the subject';
-                            $mail->Body    = 'This is the HTML message body <b>' .$row["verification_code"] .'</b>';
-                            $mail->AltBody = $row["verification_code"];
-            
-                            if($mail->send()) {
-                                echo 'Message has been sent';
-                            } else {
-                                echo 'Message could not be sent';
-                            }
+                            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+                            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+                        
+                            $mail->send();
+                            echo 'Message has been sent';
                         } catch (Exception $e) {
                             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
                         }
@@ -68,8 +77,6 @@
             
                 $stmt->close();
             }
-            
-            
         }
     }
 ?>
